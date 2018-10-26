c0235ff4397a222e70153daa2923095b6aaea6a1
DavidB59 <david.behal@net-c.com>
2018-10-26 22:31:47 +0200

    initial commit

---
+++ src/ShoppingCart.js
@@ -0,0 +1,28 @@
+
+class Cart {
+    constructor() {
+        this.array = []
+    }
+    getItems() {
+        return this.array
+    }
+    addItem(itemName, quantity, price) {
+        const item = {
+            name: itemName,
+            quantity: quantity,
+            pricePerUnit: price
+        }
+        this.array.push(item)
+    }
+    clear() {
+        this.array = []
+    }
+    total() {
+        return this.array.reduce(function (acc, va) {
+            return va.pricePerUnit * va.quantity + acc
+        }, 0)
+    }
+}
+
+
+module.exports = Cart
