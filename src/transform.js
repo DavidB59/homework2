0e7b32f1241a2abacd5a77c222eb293f3cf58760
DavidB59 <david.behal@net-c.com>
2018-10-27 00:34:27 +0200

    transform OK

--- src/transform.js
+++ src/transform.js
@@ -1,46 +1,42 @@
 
-
-
-function checkingContent(board) {
-
-  const lessThan20 = board.filter(a => a.age < 20 && a.age > 17).reduce(function (acc, cur, i) {
-    acc[i] = cur;
-    return acc;
-  }, [])
-  const twentyToThirty = board.filter(a => a.age < 30 && a.age > 20).reduce(function (acc, cur, i) {
-    acc[i] = cur;
-    return acc;
-  }, [])
-  const thirtyToForty = board.filter(a => a.age < 40 && a.age > 30).reduce(function (acc, cur, i) {
-    acc[i] = cur;
-    return acc;
-  }, [])
-  const fortyToFifty = board.filter(a => a.age < 50 && a.age > 40).reduce(function (acc, cur, i) {
-    acc[i] = cur;
-    return acc;
-  }, [])
-  const over50 = board.filter(a => a.age > 50 ).reduce(function (acc, cur, i) {
+function minAgeMaxAge (board,minAge,maxAge){
+  return board.filter(a => a.age < maxAge && a.age > minAge).reduce(function (acc, cur, i) {
     acc[i] = cur;
     return acc;
   }, [])
 }
 
 function groupAdultsByAgeRange(board) {
+  const lessThan20 = minAgeMaxAge(board,17,21)
+  const twentyToThirty = minAgeMaxAge(board,20,31)
+  const thirtyToForty = minAgeMaxAge(board,30,41)
+  const fortyToFifty = minAgeMaxAge(board,40,51)
+  const over50 = minAgeMaxAge(board,50,2000)
 
+  const result = {}
+  if (lessThan20[0] !== undefined) { result['20 and younger']=lessThan20}
+  if (twentyToThirty[0] !== undefined) { result['21-30']=twentyToThirty}
+  if (thirtyToForty[0] !== undefined) { result['31-40']=thirtyToForty}
+  if (fortyToFifty[0] !== undefined) { result['41-50']=fortyToFifty}
+  if (over50[0] !== undefined) { result['51 and older']=over50}
+
+
+
+  return result
 
 }
 
+module.exports = { groupAdultsByAgeRange }
 
 
 
-module.exports.groupAdultsByAgeRange = groupAdultsByAgeRange()
 
 
+//module.exports.groupAdultsByAgeRange = groupAdultsByAgeRange()
+
+/*
 const tableau1 = [{ name: "Anna", age: 31 }, { name: "John", age: 32 }, { name: "Hank", age: 60 }]
 
-groupAdultsByAgeRange(tableau1)
-console.log(groupAdultsByAgeRange(tableau1)
-)
 
 
 
@@ -65,3 +61,6 @@ const tableau2 = [
   { name: "oldd", age: 120 },
 ]
 
+//console.log(minAgeMaxAge(tableau2,32,37))
+
+console.log(groupAdultsByAgeRange(tableau2))*/
